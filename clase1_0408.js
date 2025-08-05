76c336981d7d863c859a890d51b029d599d34314
CodeSolions11 <codesolutions11ca@gmail.com>
2025-08-04 22:17:00 -0400

    Clase 1 - 04/08/2025

---
+++ clase1_0408.js
@@ -0,0 +1,28 @@
+// * Comentario 1
+/* 
+    * Comentario 
+    * 2
+*/
+
+// TODO Variables. 
+// * Estructura: declarar  + nombre + asignacion + tipo de dato
+// ? Declarar: var (no utilizarlo), let, const
+
+/*
+! No se recomiendo declarar las varibles con var
+var apellido = "Moreno"
+var apellido = "Rattia"
+console.log(apellido)
+*/
+
+let num = 80
+//let num = 50 // ! Error
+num = "HOLA"
+console.log(num)
+
+const nombre = "Jesus"
+// nombre = "Adrian" // ! Error
+console.log(nombre)
+
+let numero = 15; let numero2 = 30
+
