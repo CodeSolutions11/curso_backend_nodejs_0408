a9cfd09f021686a98489a450188a26035fe93009
CodeSolions11 <codesolutions11ca@gmail.com>
2025-08-11 22:37:42 -0400

    Clase 3 - 11/08/2025

---
+++ clase3_1108.js
@@ -0,0 +1,52 @@
+// TODO: Operadores Lógicos: AND &&, OR ||
+// ? AND &&
+
+// console.log(true && "17" !== 17)
+// console.log(false)
+
+const user = "JesusDev";
+const passwd = "123456"; // string
+let result = user == "JesusDev" && passwd === 123456; //number
+console.log(result);
+
+// ? OR ||
+
+console.log(true);
+
+result = true;
+console.log(result);
+
+// TODO: Modulos: export, import, import()
+
+// import nombre from "./modulos"
+const data = require("./modulos.js");
+console.log(data);
+
+// Objetos {}
+const player = {
+  name: "Mercedes",
+  life: 89,
+  ganarVida: function () {
+    return this.life + 5;
+  },
+};
+
+// let name = player.name
+let { life, name } = player; // ? desestructuracion de objetos
+
+console.log(life, name);
+
+const alumno = { 
+    nombre: "Juan", 
+    correo: "juan@gmail.com", 
+    curso: "Backend" 
+};
+
+// alumno.nombre = "Juan"
+alumno.apellido = "Farias";
+
+// let correo = alumno.correo
+let {correo} = alumno
+let email = alumno.correo
+
+console.log(alumno.apellido, email);
---
+++ modulos.js
@@ -0,0 +1,12 @@
+let nombre = "Jesus Adrian";
+let edad = 28
+let email = "jesus@gmail.com"
+
+export {
+    nombre,
+    edad,
+    email,
+}
+
+
+//export default nombre;
